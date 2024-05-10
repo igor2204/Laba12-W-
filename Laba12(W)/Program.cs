055b3bc57b09cd1a04648d1e7fdabe3fb3dec15e
Цомкалов Игорь <igorcomkalov@gmail.com>
2024-05-10 23:35:32 +0300

    Добавьте файлы проекта.

---
+++ Laba12(W)/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите строку:");
+        string inputString = Console.ReadLine();
+
+        // Регулярное выражение для поиска IP-адреса
+        string ipPattern = @"((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)";
+
+        // Поиск совпадений в строке
+        MatchCollection matches = Regex.Matches(inputString, ipPattern);
+
+        if (matches.Count > 0)
+        {
+            Console.WriteLine("Найденные IP-адреса:");
+            foreach (Match match in matches)
+            {
+                Console.WriteLine(match.Value);
+            }
+        }
+        else
+        {
+            Console.WriteLine("IP-адреса не найдены.");
+        }
+
+        Console.ReadLine();
+    }
+}
